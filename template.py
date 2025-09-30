73dd5eee8a94e19d4ecdf1ba3aad544a0c463779
Folu <ibukunmifoluke@gmail.com>
2025-09-30 22:54:20 +0100

    added new files

---
+++ template.py
@@ -0,0 +1,20 @@
+import os
+from pathlib import Path
+
+list_of_files = [
+    "metadata.csv",
+    "analysis_ipynb",
+    "app.py",
+    "requirements.txt",
+    "images/viisualization.png",
+]
+
+for files in list_of_files:
+    file_path = Path(files)
+    filedir, filename = os.path.split(file_path)
+    if filedir and not os.path.exists(filedir):
+        os.makedirs(filedir)
+
+    if (not os.path.exists(file_path)) or (os.path.getsize(file_path) == 0):
+        with open(file_path, 'w') as fp:
+            pass
